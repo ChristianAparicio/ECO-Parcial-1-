6d142e3025a3fcb9f3f054777b903fd8b22fb73c
ChristianAparicio <81696405+ChristianAparicio@users.noreply.github.com>
2022-09-09 08:41:20 -0500

    Katy Perry
    Im truly sorry and a little bit ashame about the results of this exam, with all the pain of my heart I am Ietting down Katy perry by submitting this basic response.
    
    have fun

---
+++ exam-apps-eco-snake/public-display/sketch.js
@@ -0,0 +1,81 @@
+const URL = `http://${window.location.hostname}:5050`;
+let socket = io(URL, { path: '/real-time' });
+
+let character = {
+    x: 0,
+    y: 0
+};
+let whiteMouse = {
+    x: 50,
+    y: 50
+};
+let speed = 10;
+
+function setup() {
+    frameRate(60);
+    createCanvas(windowWidth, windowHeight);
+    character.x = windowWidth / 2;
+    character.y = windowHeight / 2;
+}
+
+function draw() {
+    background(0, 50);
+    textSize(64);
+    text('🐍', character.x - 25, character.y);
+    textSize(24);
+    text('🐁', whiteMouse.x, whiteMouse.y);
+    eatsMouse();
+}
+
+
+function eatsMouse() {
+    if (dist(character.x, character.y, whiteMouse.x, whiteMouse.y) < 50) {
+        putMouseRandomPosition();
+    }
+}
+
+function putMouseRandomPosition() {
+    whiteMouse.x = random(50, windowWidth - 50);
+    whiteMouse.y = random(50, windowHeight - 50);
+}
+
+/*___________________________________________
+
+1) Include the socket method to listen to events and change the character position.
+You may want to use a Switch structure to listen for up, down, right and left cases.
+_____________________________________________ */
+
+
+function KeyPressed(){
+    switch(KeyCode){
+
+        case UP_ARROW:
+            character.x= character.x-20;
+
+            break;
+
+            case DOWN_ARROW: 
+            character.x= character.x+20;
+
+            break;
+
+            case LEFT_ARROW:
+                character.y= character.y-20
+
+                break;
+
+                case RIGHT_ARROW:
+                    character.y= character.y+20
+    }
+}
+
+KeyPressed.emit()
+
+/*___________________________________________
+
+
+
+2) Include the fetch method to post each time the snake eats a mouse
+_____________________________________________ */
+fetch("http://localhost:5050")
+
